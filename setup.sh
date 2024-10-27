c60f6ef4cf76560822facfbcfd20a1198453f142
winterhku <ruidong@hku.hk>
2024-10-27 13:43:41 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,12 +2,13 @@ mkdir -p ~/.streamlit/
 
 echo "\
 [general]
-email = "rzhaoag@connect.ust.hk"
+email = \"rzhaoag@connect.ust.hk\"
 " > ~/.streamlit/credentials.toml
 
 echo "\
 [server]
 headless = true
 port = $PORT
-enableCORS = false
+enableCORS = true
+enableXsrfProtection = false
 " > ~/.streamlit/config.toml
