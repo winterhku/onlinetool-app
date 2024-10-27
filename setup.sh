f9521500b2da4c46aa7accfc4e4471af3f0cce59
winterhku <ruidong@hku.hk>
2024-10-27 12:52:25 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
+#!/bin/bash
+
 mkdir -p ~/.streamlit/
-echo“\ 
-[general] \n\ 
-email = \” rzhaoag@connect.ust.hk \“\n\ 
-” > ~/.streamlit/credentials.toml
-echo "\ 
-[服务器]\n\ 
-headless = true\n\ 
-enableCORS=false\n\ 
-port = $PORT\n\ 
+
+echo "[general]
+email = \"rzhaoag@connect.ust.hk\"
+" > ~/.streamlit/credentials.toml
+
+echo "[server]
+headless = true
+enableCORS = false
+port = \$PORT
 " > ~/.streamlit/config.toml
